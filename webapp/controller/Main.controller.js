5f240ec1c639931b29d5d42d4753e1581345f562
bas08 clouddnagmbhgmail.onmicrosoft.com <bas08@clouddnagmbhgmail.onmicrosoft.com>
2023-11-27 15:55:16 +0000

    Main View hinzugefügt

---
+++ webapp/controller/Main.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+  ], function(Controller) {
+    "use strict";
+  
+    return Controller.extend("de.huberit.training08.zhoui5.controller.Main", {
+      onInit: function() {
+        
+      },
+    });
+  });

4a7f78543c9d218a406fdc066f5c18cb8e043232
bas08 clouddnagmbhgmail.onmicrosoft.com <bas08@clouddnagmbhgmail.onmicrosoft.com>
2023-11-29 09:03:27 +0000

    Commit 28.11.23

--- webapp/controller/Customer.controller.js
+++ webapp/controller/Customer.controller.js
@@ -1,15 +1,30 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageBox",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/core/Fragment"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, MessageBox, JSONModel, Fragment) {
         "use strict";
 
         return Controller.extend("de.huberit.training08.zhoui5.controller.Customer", {
             onInit: function () {
-
-            }
+                this.getOwnerComponent().getRouter().getRoute("Customer").attachPatternMatched(this._onPatternMatched, this);
+                const oEditModel = new JSONModel({
+                    editmode: false
+                });
+            
+            },
+            _onPatternMatched: function (oEvent) {
+                let path = oEvent.getParameters().arguments["path"];
+                this.getView().bindElement(decodeURIComponent(path));
+            },
+            dateFormatter: function (sDate) {
+                let dateObj = new Date(sDate);
+                return dateObj.getDate() + "." + (dateObj.getMonth() + 1) + "." + dateObj.getFullYear();
+            },
         });
     });
--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,11 +1,24 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-  ], function(Controller) {
-    "use strict";
-  
-    return Controller.extend("de.huberit.training08.zhoui5.controller.Main", {
-      onInit: function() {
-        
-      },
-    });
+  "sap/ui/core/mvc/Controller"
+], function (Controller) {
+  "use strict";
+
+  return Controller.extend("de.huberit.training08.zhoui5.controller.Main", {
+    onInit: function () {
+
+    },
+    genderFormatter: function (sKey) {
+      return this.getView().getModel("i18n").getResourceBundle().getText(sKey);
+    },
+    dateFormatter: function (sDate) {
+      let dateObj = new Date(sDate);
+      return dateObj.getDate() + "." + (dateObj.getMonth() + 1) + "." + dateObj.getFullYear();
+    },
+    onListItemClicked: function (oEvent) {
+      const sPath = oEvent.getSource().getBindingContext().getPath();
+      this.getOwnerComponent().getRouter().navTo("Customer", {
+        path: encodeURIComponent(sPath)
+      }, false);
+    }
   });
+});
